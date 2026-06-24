b3cf1804c0b83caf64d865ad84d0dcaaaacd9876
SeleznevAS-dev <its0a0magic0time@gmail.com>
2026-06-24 23:11:08 +0300

    Add task 3 solution

---
+++ tasks/task-3.sql
@@ -0,0 +1,101 @@
+-- 2
+SELECT
+    d.dwarf_id,
+    d.name,
+    d.age,
+    d.profession,
+    JSON_OBJECT(
+        'skill_ids', (
+            SELECT JSON_ARRAYAGG(s.skill_id)
+            FROM skills s
+            JOIN dwarf_skills ds
+            ON d.dwarf_id = ds.dwarf_id
+            AND s.skill_id = ds.skill_id
+            WHERE ds.dwarf_id = d.dwarf_id
+        ),
+        'assignment_ids', (
+            SELECT JSON_ARRAYAGG(da.assignment_id)
+            FROM dwarf_assignments da
+            WHERE da.dwarf_id = d.dwarf_id
+        ),
+        'squad_ids', (
+            SELECT JSON_ARRAYAGG(sm.squad_id)
+            FROM squad_members sm
+            WHERE sm.dwarf_id = d.dwarf_id
+        ),
+        'equipment_ids', (
+            SELECT JSON_ARRAYAGG(de.equipment_id)
+            FROM dwarf_equipment de
+            WHERE de.dwarf_id = d.dwarf_id
+        )
+    ) AS related_entities
+FROM
+    dwarves d
+
+-- 3
+SELECT
+    w.workshop_id,
+    w.name,
+    w.type,
+    w.quality,
+    JSON_OBJECT(
+        'craftsdwarf_ids', (
+            SELECT JSON_ARRAYAGG(wc.dwarf_id)
+            FROM workshop_craftsdwarves wc
+            WHERE wc.workshop_id = w.workshop_id
+        ),
+        'project_ids', (
+            SELECT JSON_ARRAYAGG(p.project_id)
+            FROM projects p
+            WHERE p.workshop_id = w.workshop_id
+        ),
+        'input_material_ids', (
+            SELECT JSON_ARRAYAGG(wm.material_id)
+            FROM workshop_materials wm
+            WHERE wm.workshop_id = w.workshop_id
+            AND is_input IS TRUE
+        ),
+        'output_product_ids', (
+            SELECT JSON_ARRAYAGG(wp.product_id)
+            FROM workshop_products wp
+            WHERE wp.workshop_id = w.workshop_id
+        )
+    ) AS related_entities
+FROM
+    workshops w
+
+-- 4
+SELECT
+    ms.squad_id,
+    ms.name,
+    ms.formation_type,
+    ms.leader_id,
+    JSON_OBJECT(
+        'member_ids', (
+            SELECT JSON_ARRAYAGG(sm.dwarf_id)
+            FROM squad_members sm
+            WHERE sm.squad_id = ms.squad_id
+        ),
+        'equipment_ids', (
+            SELECT JSON_ARRAYAGG(se.equipment_id)
+            FROM squad_equipment se
+            WHERE se.squad_id = ms.squad_id
+        ),
+        'operation_ids', (
+            SELECT JSON_ARRAYAGG(so.operation_id)
+            FROM squad_operations so
+            WHERE so.squad_id = ms.squad_id
+        ),
+        'training_schedule_ids', (
+            SELECT JSON_ARRAYAGG(st.schedule_id)
+            FROM squad_training st
+            WHERE st.squad_id = ms.squad_id
+        ),
+        'battle_report_ids', (
+            SELECT JSON_ARRAYAGG(sb.report_id)
+            FROM squad_battles sb
+            WHERE sb.squad_id = ms.squad_id
+        )
+    ) AS related_entities
+FROM
+    military_squads ms
